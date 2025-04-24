534d5ccf2b065d71f992e0c27bceda2e3f6bed4a
ALIGHASSOUN <alighassoun9@gmail.com>
2025-04-24 18:57:45 +0300

    first-commit

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,165 @@
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import {
+  Eye,
+  EyeOff,
+  Mail,
+  MessageSquare,
+  User,
+  Lock,
+  Loader2,
+} from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattren from "../components/AuthImagePattren";
+import toast from "react-hot-toast";
+
+const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("email name is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email))
+      return toast.error("Invalid email format");
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6)
+      return toast.error("Password must be at least 6 characters");
+
+    return true;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const success = validateForm();
+    if (success === true) signup(formData);
+  };
+
+  return (
+    <div className="min-h-screen grid lg:grid-cols-2">
+      {/* left side */}
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          {/* LOGO */}
+          <div className="text-center mb-8">
+            <div className="flex flex-col items-center gap-2 group">
+              <div
+                className="size-12 rounded-xl bg-primary/10 flex items-center justify-center 
+              group-hover:bg-primary/20 transition-colors"
+              >
+                <MessageSquare className="size-6 text-primary" />
+              </div>
+              <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+              <p className="text-base-content/60">
+                Get started with your free account
+              </p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 w-full">
+                <User className="w-5 h-5 text-gray-500" />
+                <input
+                  type="text"
+                  className="grow"
+                  placeholder="John Doe"
+                  value={formData.fullName}
+                  onChange={(e) =>
+                    setFormData({ ...formData, fullName: e.target.value })
+                  }
+                />
+              </label>
+            </div>
+
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 w-full">
+                <Mail className="w-5 h-5 text-gray-500" />
+                <input
+                  type="email"
+                  className="grow"
+                  placeholder="example@gmail.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </label>
+            </div>
+
+            <div className="form-control ">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+
+              <label className="input input-bordered flex items-center gap-2 w-full">
+                <Lock className="w-5 h-5 text-gray-500" />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="grow"
+                  placeholder="••••••••"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+                <button
+                  type="button"
+                  className="text-gray-500 hover:text-gray-700"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className="w-5 h-5" />
+                  ) : (
+                    <Eye className="w-5 h-5" />
+                  )}
+                </button>
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={isSigningUp}
+            >
+              {isSigningUp ? (
+                <>
+                  <Loader2 className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
+          </form>
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Already have an account?{" "}
+              <Link to="/login" className="link link-primary">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* right side */}
+      <AuthImagePattren
+        title="Join our community"
+        subtitle="Connect with friends, share moments, and stay in touch with your friends"
+      />
+    </div>
+  );
+};
+
+export default SignUpPage;
